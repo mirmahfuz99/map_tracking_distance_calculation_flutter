4ab9d2c34c0bcb3a3b238f7879efc8d54fbac6a9
mirmahfuz99 <mirmahfuz10@gmail.com>
2024-05-12 08:26:52 +0600

    ui populated with getx design pattern

--- lib/controller/map_tracking_controller.dart
+++ lib/controller/map_tracking_controller.dart
@@ -1,3 +1,4 @@
+import 'dart:math';
 import 'package:flutter/material.dart';
 import 'package:flutter_polyline_points/flutter_polyline_points.dart';
 import 'package:get/get.dart';
@@ -7,22 +8,23 @@ import 'package:map_tracking_distance_calculation_flutter_bloc/utils/constants.d
 
 class MapTrackingController extends GetxController implements GetxService {
 
-  LatLng _sourceLocation = const LatLng(23.76852131771742, 90.3673231832651); // initial dummy location
+  LatLng _sourceLocation = const LatLng(23.76852131771742, 90.3673231832651); // initial dummy source location
   LatLng get sourceLocation => _sourceLocation;
 
 
-  LatLng _destination = const LatLng(23.989312321830415,  90.3821892500332); // initial dummy location
+  LatLng _destination = const LatLng(23.989312321830415,  90.3821892500332); // initial dummy destination location
   LatLng get destination => _destination;
 
 
 
   LocationData? _currentLocation;
-  LocationData get currentLocation => _currentLocation!;
+  LocationData? get currentLocation => _currentLocation;
 
-  Map<MarkerId, Marker>? _markers;
+  final Map<MarkerId, Marker> _markers = {};
+  Map<MarkerId, Marker>? get markers => _markers;
 
-  Map<PolylineId, Polyline>? _polylines;
-  Map<PolylineId, Polyline> get polylines => _polylines!;
+  final Map<PolylineId, Polyline> _polylines = {};
+  Map<PolylineId, Polyline> get polylines => _polylines;
 
   List<LatLng>? _polylineCoordinates;
   List<LatLng> get polylineCoordinates => _polylineCoordinates!;
@@ -35,7 +37,7 @@ class MapTrackingController extends GetxController implements GetxService {
 
     location.getLocation().then((location) {
       _currentLocation = location;
-      _sourceLocation =  LatLng(currentLocation.latitude!, currentLocation.longitude!);
+      _sourceLocation =  LatLng(currentLocation!.latitude!, currentLocation!.longitude!);
     });
 
     location.onLocationChanged.listen((event) {
@@ -47,9 +49,8 @@ class MapTrackingController extends GetxController implements GetxService {
   //todo: will be called from start tracking
   void addMarker(LatLng position, String id, BitmapDescriptor descriptor) {
     MarkerId markerId = MarkerId(id);
-    Marker marker =
-    Marker(markerId: markerId, icon: descriptor, position: position);
-    _markers![markerId] = marker;
+    Marker marker = Marker(markerId: markerId, icon: descriptor, position: position);
+    _markers[markerId] = marker;
     update();
   }
 
@@ -62,11 +63,10 @@ class MapTrackingController extends GetxController implements GetxService {
         // const PointLatLng(23.76852131771742, 90.3673231832651), // to test polyline works fine
         travelMode: TravelMode.driving);
 
-    print(result.points);
+    // print(result.points);
 
     if (result.points.isNotEmpty) {
-      _polylineCoordinates!.clear();
-      print("inside_result_not_empty");
+      _polylineCoordinates = [];
       result.points.forEach((PointLatLng point) {
         _polylineCoordinates!.add(LatLng(point.latitude, point.longitude));
       });
@@ -74,17 +74,31 @@ class MapTrackingController extends GetxController implements GetxService {
     _addPolyLine();
   }
 
-
   _addPolyLine() {
     PolylineId id = const PolylineId("poly");
     Polyline polyline = Polyline(polylineId: id, color: Colors.red, points: _polylineCoordinates!);
-    _polylines![id] = polyline;
+    _polylines[id] = polyline;
     update();
   }
 
   void addDestination(LatLng latLng){
     _destination = LatLng(latLng.latitude, latLng.longitude);
     update();
+  }
+
+  double calculateDistance(){
+
+    var lat1 = _currentLocation!.latitude;
+    var lon1 = _currentLocation!.longitude;
+    var lat2 = _destination.latitude;
+    var lon2 = _destination.longitude;
+
+    var p = 0.017453292519943295;
+    var c = cos;
+    var a = 0.5 - c((lat2 - lat1!) * p)/2 +
+        c(lat1 * p) * c(lat2 * p) *
+            (1 - c((lon2 - lon1!) * p))/2;
+    return 12742 * asin(sqrt(a));
   }
 
 }
--- lib/view/screens/map_location_tracking.dart
+++ lib/view/screens/map_location_tracking.dart
@@ -1,15 +1,11 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_polyline_points/flutter_polyline_points.dart';
 import 'package:get/get.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
-import 'package:location/location.dart';
 import 'package:map_tracking_distance_calculation_flutter_bloc/controller/map_tracking_controller.dart';
-import 'package:map_tracking_distance_calculation_flutter_bloc/utils/constants.dart';
 import 'package:map_tracking_distance_calculation_flutter_bloc/utils/dimensions.dart';
 import 'package:map_tracking_distance_calculation_flutter_bloc/utils/images.dart';
 import 'package:map_tracking_distance_calculation_flutter_bloc/utils/styles.dart';
 import 'package:map_tracking_distance_calculation_flutter_bloc/view/base/custom_button.dart';
-import 'dart:math' show cos, sqrt, asin;
 
 
 class MapLocationTrackingScreen extends StatefulWidget {
@@ -21,54 +17,16 @@ class MapLocationTrackingScreen extends StatefulWidget {
 
 class _MapScreenState extends State<MapLocationTrackingScreen> {
   late GoogleMapController mapController;
-  static LatLng sourceLocation = LatLng(23.76852131771742, 90.3673231832651); // initial dummy location
-  static LatLng destination = LatLng(23.989312321830415,  90.3821892500332); // Destination Point B will be selected with Search
-
-
-  // double _originLatitude = 26.48424, _originLongitude = 50.04551;
-  // double _destLatitude = 26.46423, _destLongitude = 50.06358;
-  Map<MarkerId, Marker> markers = {};
-  Map<PolylineId, Polyline> polylines = {};
-  List<LatLng> polylineCoordinates = [];
-  PolylinePoints polylinePoints = PolylinePoints();
-  String googleAPiKey = google_api_key;
-
-
-  LocationData? currentLocation;
-
-
-  @override
-  void initState() {
-    super.initState();
-
-
-
-
-    // getCurrentLocation();
-  }
-
-  void getCurrentLocation () async {
-    Location location = Location();
-
-
-    location.getLocation().then((location) {
-      currentLocation = location;
-      sourceLocation =  LatLng(currentLocation!.latitude!, currentLocation!.longitude!);
-      setState(() {});
-    });
-
-
-    location.onLocationChanged.listen((event) {
-      currentLocation = event;
-      // _getPolyline();
-      setState(() {});
-    });
-  }
 
   @override
   Widget build(BuildContext context) {
     return GetBuilder<MapTrackingController>(
       builder: (mapTrackingController){
+        double distance = 0.0;
+        if(mapTrackingController.currentLocation != null){
+          distance = mapTrackingController.calculateDistance();
+        }
+
         return SafeArea(
           child: Scaffold(
               extendBodyBehindAppBar: true,
@@ -79,30 +37,21 @@ class _MapScreenState extends State<MapLocationTrackingScreen> {
                       height: constraints.maxHeight / 1.5,
                       child: GoogleMap(
                         initialCameraPosition: CameraPosition(
-                            target: sourceLocation, zoom: 15),
+                            target: mapTrackingController.sourceLocation, zoom: 15),
                         myLocationEnabled: true,
                         tiltGesturesEnabled: true,
                         compassEnabled: true,
                         scrollGesturesEnabled: true,
                         zoomGesturesEnabled: true,
                         onMapCreated: _onMapCreated,
-                        markers: Set<Marker>.of(markers.values),
-                        polylines: Set<Polyline>.of(polylines.values),
-                        onLongPress: (latlang){
-                          destination = LatLng(latlang.latitude, latlang.longitude); //
+                        markers: Set<Marker>.of(mapTrackingController.markers!.values),
+                        polylines: Set<Polyline>.of(mapTrackingController.polylines.values),
+                        onLongPress: (latLng){
+                          mapTrackingController.addDestination(latLng);
                           /// destination marker
-                          _addMarker(destination, "destination",
+                          mapTrackingController.addMarker(mapTrackingController.destination, "destination",
                             BitmapDescriptor.defaultMarker,
-                            // BitmapDescriptor.defaultMarkerWithHue(90)
                           );
-
-                          double destance =  calculateDistance(currentLocation!.latitude, currentLocation!.longitude, destination.latitude, destination.longitude);
-
-                          print("distance: $destance km");
-
-
-
-                          setState(() {});
                         },
                       ),
                     );
@@ -110,7 +59,6 @@ class _MapScreenState extends State<MapLocationTrackingScreen> {
 
                   DraggableScrollableSheet(
                       initialChildSize: 0.3,
-
                       builder: (BuildContext context, ScrollController scrollController){
 
                         return Container(
@@ -120,25 +68,16 @@ class _MapScreenState extends State<MapLocationTrackingScreen> {
                             child: Column(
                               crossAxisAlignment: CrossAxisAlignment.start,
                               children: [
+                                const Center(child: Text("You Can Start Tracking By Click Start Tracking !", style: robotoMedium,)),
+                                const SizedBox(height: Dimensions.paddingSizeDefault,),
                                 const Text("Distance Between Point A & Point B",style: robotoBold,),
-                                const SizedBox(height: Dimensions.paddingSizeLarge,),
-
-
+                                const SizedBox(height: Dimensions.paddingSizeDefault,),
                                 Row(
-                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  crossAxisAlignment: CrossAxisAlignment.center,
                                   children: [
                                     Image.asset(Images.distance,scale: 3,),
                                     const SizedBox(width: Dimensions.paddingSizeExtraLarge,),
-                                    const Column(
-                                      crossAxisAlignment: CrossAxisAlignment.start,
-
-                                      children: [
-                                        Text("Short Distance 34.4 km",style: robotoMedium,),
-                                        Text("Medium Distance 34.4 km",style: robotoMedium,),
-                                        Text("Long Distance 34.4 km",style: robotoMedium,),
-                                      ],
-                                    )
-
+                                    Expanded(child: Text("Distance Between Current Location to Destination :  ${distance.toPrecision(2)} km",style: robotoMedium,)),
                                   ],
                                 ),
 
@@ -153,9 +92,9 @@ class _MapScreenState extends State<MapLocationTrackingScreen> {
                                       color: Theme.of(context).textTheme.bodyLarge!.color!.withOpacity(.1),
                                       width: MediaQuery.of(context).size.width / 2.2,
                                       onPressed: (){
-                                        getCurrentLocation();
-                                        /// origin marker point A
-                                        _addMarker(sourceLocation, "origin", BitmapDescriptor.defaultMarker);
+                                        mapTrackingController.getCurrentLocation();
+                                        /// source marker point A
+                                        mapTrackingController.addMarker(mapTrackingController.sourceLocation, "origin", BitmapDescriptor.defaultMarker);
                                       },
                                       buttonText: "Start Tracking",
                                     ),
@@ -166,18 +105,16 @@ class _MapScreenState extends State<MapLocationTrackingScreen> {
                                       color: Theme.of(context).textTheme.bodyLarge!.color!.withOpacity(.1),
                                       width: MediaQuery.of(context).size.width / 2.2,
                                       onPressed: (){
-                                        polylineCoordinates.clear();
+                                        mapTrackingController.polylineCoordinates.clear();
                                       },
                                       buttonText: "End Tracking",
                                     )
                                   ],
                                 ),
-                                const Text("You Can Start Tracking By Click Start Tracking !", style: robotoMedium,)
                               ],
                             ),
                           ),
                         );
-
                       })
                 ],
               )
@@ -187,51 +124,8 @@ class _MapScreenState extends State<MapLocationTrackingScreen> {
     );
   }
 
-  double calculateDistance(lat1, lon1, lat2, lon2){
-    var p = 0.017453292519943295;
-    var c = cos;
-    var a = 0.5 - c((lat2 - lat1) * p)/2 +
-        c(lat1 * p) * c(lat2 * p) *
-            (1 - c((lon2 - lon1) * p))/2;
-    return 12742 * asin(sqrt(a));
-  }
-
 
   void _onMapCreated(GoogleMapController controller) async {
     mapController = controller;
-  }
-
-  _addMarker(LatLng position, String id, BitmapDescriptor descriptor) {
-    MarkerId markerId = MarkerId(id);
-    Marker marker =
-    Marker(markerId: markerId, icon: descriptor, position: position);
-    markers[markerId] = marker;
-  }
-
-  _addPolyLine() {
-    PolylineId id = const PolylineId("poly");
-    Polyline polyline = Polyline(polylineId: id, color: Colors.red, points: polylineCoordinates);
-    polylines[id] = polyline;
-    setState(() {});
-  }
-
-  _getPolyline() async {
-    PolylineResult result = await polylinePoints.getRouteBetweenCoordinates(
-        google_api_key,
-        PointLatLng(sourceLocation.latitude, sourceLocation.longitude),
-        PointLatLng(currentLocation!.latitude!, currentLocation!.longitude!), // real time updated current location lat & long
-        // const PointLatLng(23.76852131771742, 90.3673231832651), // to test polyline works fine
-        travelMode: TravelMode.driving);
-
-    print(result.points);
-
-    if (result.points.isNotEmpty) {
-      polylineCoordinates.clear();
-      print("inside_result_not_empty");
-      result.points.forEach((PointLatLng point) {
-        polylineCoordinates.add(LatLng(point.latitude, point.longitude));
-      });
-    }
-    _addPolyLine();
   }
 }
